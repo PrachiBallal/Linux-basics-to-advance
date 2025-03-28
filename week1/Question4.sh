16120501b8a540620ce7284d33cf24c146f1f823
Prachi Ballal <55234413+PrachiBallal@users.noreply.github.com>
2025-03-29 00:18:53 +0530

    Create Question4.sh

---
+++ week1/Question4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Solution for Question 4: Working with `cat` and `echo`
+
+cat > notes.txt  # (Type some text and press Ctrl + D to save)
+cat >> notes.txt  # (Type additional text and press Ctrl + D to save)
+cat notes.txt
+echo "Learning Linux is fun!"
