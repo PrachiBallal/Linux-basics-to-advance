da81a822221596623a759bd0c6a1c66f2db6325e
Prachi Ballal <55234413+PrachiBallal@users.noreply.github.com>
2025-03-29 00:18:28 +0530

    Create Question3.sh

---
+++ week1/Question3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Solution for Question 3: Using `grep` for Text Search
+
+echo -e "Linux is powerful.\nLinux is open-source.\nLinux is widely used." > sample.txt
+grep "open-source" sample.txt
